5db509be34939b43d327b5b5bcf5b744007bfcb8
akilinjamam <akilinjamam@gmail.comm>
2022-02-19 18:04:02 +0600

    updated

--- dom.js
+++ dom.js
@@ -10,3 +10,20 @@ function friendBackground() {
     }
 
 }
+
+
+const newFriend = document.getElementById('new-friend').addEventListener('click', function () {
+
+    const mainDiv = document.getElementById('friend-container');
+
+    const newDiv = document.createElement('div');
+
+    newDiv.classList.add('friend');
+
+    newDiv.innerHTML = `
+    <h3 class="friends-name">friend-4</h3>
+    <p>Quas, cumque.</p>
+    `
+
+    mainDiv.appendChild(newDiv);
+})
